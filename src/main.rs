47f033b32ca6a385bee87543196fb79505e51a85
khalidzahra <khalidzahra923@gmail.com>
2025-03-14 23:51:27 -0500

    Add tilde support to ls

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,33 @@ fn help() {
     println!("Available commands: {}", COMMAND_LIST.join(", "));
 }
 
+fn ls(path: &str) -> std::io::Result<()> {
+    // tilde support
+    let new_path = if path.contains("~") {
+        let home = match home::home_dir() {
+            Some(path) => path.display().to_string(),
+            _ => String::from("/"),
+        };
+        path.replace("~", &home)
+    } else {
+        path.to_string()
+    };
+
+    let entries = fs::read_dir(&new_path)?;
+    for entry in entries {
+        let entry = entry?;
+        let path_buf = entry.path();
+
+        // take out prefixes
+        let display_path = path_buf.display().to_string();
+        let display_path = display_path.trim_start_matches(&new_path).trim_start_matches("/");
+
+        print!("{} ", display_path);
+    }
+    println!("\n");
+    Ok(())
+}
+
 fn handle_command(command: &str) {
     let mut parts = command.split_whitespace();
     let cmd = parts.next().unwrap_or("");
@@ -24,6 +51,10 @@ fn handle_command(command: &str) {
     match cmd {
         "exit" => exit(0),
         "help" => help(),
+        "ls" => {
+            let path = args.get(0).unwrap_or(&".");
+            ls(path).unwrap();
+        },
         _ => println!("Unknown command: {}", cmd),
     }
 }
